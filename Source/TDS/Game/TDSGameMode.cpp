15be1a6a8c81ae627432d78c1cd49c0bfc48edab
Leonid Fedorov <leopoldikus@yahoo.com>
2025-08-18 12:23:52 +0300

    Fix GameMode defaults; ignore backup folder

--- Source/TDS/Game/TDSGameMode.cpp
+++ Source/TDS/Game/TDSGameMode.cpp
@@ -3,17 +3,18 @@
 #include "TDSGameMode.h"
 #include "TDSPlayerController.h"
 #include "TDS/Character/TDSCharacter.h"
-#include "UObject/ConstructorHelpers.h"
+//#include "UObject/ConstructorHelpers.h"
 
 ATDSGameMode::ATDSGameMode()
 {
+	DefaultPawnClass = ATDSCharacter::StaticClass();
 	// use our custom PlayerController class
 	PlayerControllerClass = ATDSPlayerController::StaticClass();
 
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/BP/Character/TopDownCharacter"));
+	/*static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/BP/Character/TopDownCharacter"));
 	if (PlayerPawnBPClass.Class != nullptr)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+	}*/
 }
