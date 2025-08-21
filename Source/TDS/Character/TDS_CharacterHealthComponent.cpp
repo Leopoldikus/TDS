108b1e75e1c3b46b3bd09842c2b37aa72cec8023
Leonid Fedorov <leopoldikus@yahoo.com>
2025-08-21 11:50:26 +0300

    Gameplay code: health components; update project file

---
+++ Source/TDS/Character/TDS_CharacterHealthComponent.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TDS_CharacterHealthComponent.h"
+
+void UTDS_CharacterHealthComponent::RecieveDamage(float Damage)
+{
+	Super::RecieveDamage(Damage);
+	//for Char
+}
---
+++ Source/TDS/Character/TDS_CharacterHealthComponent.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TDSHealthComponent.h"
+#include "TDS_CharacterHealthComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TDS_API UTDS_CharacterHealthComponent : public UTDSHealthComponent
+{
+	GENERATED_BODY()
+
+public:
+
+	
+	void RecieveDamage(float Damage) override;
+	
+};
